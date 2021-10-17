e2c666419fc6511f08d1aec7dc099327f5669507
Jul <83681256+JulioDTR@users.noreply.github.com>
2021-10-18 00:18:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import discord
+# Modules
+import discord 
 from discord.ext import commands
 import datetime
 
 from urllib import parse, request
 import re
 
-bot = commands.Bot(command_prefix='!', description="Description")
+# Bot
+bot = commands.Bot(command_prefix='!', description="Description") # remplace Description par ce que tu veux
 
+# Commande d'infos sur le serveur
 @bot.command()
-async def commande(ctx):
+async def commande(ctx): # remplace commande par ce que tu veux
     embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.add_field(name="Serveur crée le", value=f"{ctx.guild.created_at}")
     embed.add_field(name="Fondateur du serveur", value=f"{ctx.guild.owner}")
     embed.add_field(name="Région du serveur", value=f"{ctx.guild.region}")
     embed.add_field(name="ID du serveur", value=f"{ctx.guild.id}")
-    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    embed.set_thumbnail(url=f"{ctx.guild.icon}")
     embed.set_thumbnail(url="image")
