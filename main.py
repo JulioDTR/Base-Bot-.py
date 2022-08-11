bc8f7d9c4bec7c0706db7ec12f99f98c31851ca8
Jul <83681256+JulioDTR@users.noreply.github.com>
2022-08-11 19:03:12 +0200

    Mise à jour

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-# Modules
 import discord 
 from discord.ext import commands
-import datetime
 
-from urllib import parse, request
-import re
-
-# Bot
 bot = commands.Bot(command_prefix='!', description="Description")
-
-
-@bot.command()
-async def commande(ctx): # remplace commande par ce que tu veux
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-    embed.add_field(name="Serveur crée le", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Fondateur du serveur", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Région du serveur", value=f"{ctx.guild.region}")
-    embed.add_field(name="ID du serveur", value=f"{ctx.guild.id}")
-    embed.set_thumbnail(url=f"{ctx.guild.icon}")
-    embed.set_thumbnail(url="image")
 
 bot.run('token')
