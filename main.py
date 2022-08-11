69facccd5807ce567adb76f6164f43f96416e880
Jul <83681256+JulioDTR@users.noreply.github.com>
2022-08-11 19:08:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', description="Description")
+bot = commands.Bot(help_command=None) # supprime la commande de base help
 
 bot.run('token')
